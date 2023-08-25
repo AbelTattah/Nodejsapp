5f2d2a9037368d56fcdc4c18c944bd96a13c8431
AbelTattah <abeltattah@gmail.com>
2023-08-24 22:43:59 -0700

    Fixed Student Update again

--- index.js
+++ index.js
@@ -109,10 +109,10 @@ app.post ('/studentupdate', async(req,res) =>
    }
 })
 
-app.get('/studentcomment',async(req,res)=>{
+app.get('/studentupdates',async(req,res)=>{
     try{
-const products = await Studentupdate.find({});
-res.status(200).json(products);
+        const product = await Studentupdate.create(req.body)
+        res.status(200).json(product);
     }
     catch (error) {
         res.status(500).json({message:error.message})
